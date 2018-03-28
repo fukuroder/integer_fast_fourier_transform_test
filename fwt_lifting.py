8b825e906619a130540b7777c021ddc2bf0bb80a
fukuroda <fukuroder@live.jp>
2018-03-29 01:04:05 +0900

    py3

--- fwt_lifting.py
+++ fwt_lifting.py
@@ -3,20 +3,20 @@
 # input
 s0 = 3
 s1 = 6
-print "[input]"
-print "s0_0 =", s0
-print "s0_1 =", s1
+print("[input]")
+print("s0_0 =", s0)
+print("s0_1 =", s1)
 
 # decompose
 s1 -= s0
 s0 += s1/2.0
-print "[decompose]"
-print "s1_0 =", s0
-print "d1_0 =", s1
+print("[decompose]")
+print("s1_0 =", s0)
+print("d1_0 =", s1)
 
 # reconstruct
 s0 -= s1/2.0
 s1 += s0
-print "[reconstruct]"
-print "s0_0 =", s0
-print "s0_1 =", s1
+print("[reconstruct]")
+print("s0_0 =", s0)
+print("s0_1 =", s1)
